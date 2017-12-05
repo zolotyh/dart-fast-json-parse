49042c5a2bc69b049f803e148bf7336b1b027af5
Alexey Zolotykh <alexey.zolotykh@team.wrike.com>
2017-12-05 09:51:23 +0300

    add interface

--- lib/lib.dart
+++ lib/lib.dart
@@ -4,10 +4,17 @@ library interopt;
 import "package:js/js.dart";
 
 @JS()
-class Task {
+class Task implements TaskI {
 	external int get id;
 	external String get title;
 	external List<Task> get subtaskList;
+}
+
+abstract class TaskI {
+	final int id;
+	final String title;
+	final List<Task> subtaskList;
+	TaskI(this.id, this.title, this.subtaskList);
 }
 
 
--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 import 'package:dal3/lib.dart';
 
 void main() {
-	List<Task> list = jsonParse('[{"id": 1, "title": "test", "subtaskList": [{"id": 2, "title": "hahahahahahahah"}]}]');
+	List<TaskI> list = jsonParse('[{"id": 1, "title": "test", "subtaskList": [{"id": 2, "title": "hahahahahahahah"}]}]');
 	print(list[0].title);
 	print(list[0].subtaskList[0].title);
 }
